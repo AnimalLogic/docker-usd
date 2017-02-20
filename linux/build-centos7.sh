857d903b8ecec4ed7d72626c218875d89abf62cc
Daniel Barry <danielbar@al.com.au>
2017-02-21 10:20:52 +1100

    Yum installing libraries that were needed by maya2017

--- linux/build-centos7.sh
+++ linux/build-centos7.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-# Copy local root certificates for corporate networks
+echo "Copy local root certificates for corporate networks"
 [ -e /etc/pki/ca-trust/source/anchors ] && cp -u /etc/pki/ca-trust/source/anchors/* cert/
 
-# Build base: base centos packages and gcc
+echo "Build base: base centos packages and gcc"
 docker build -t "usd-docker/base:centos7-usd-0.7" -f centos7/base/Dockerfile .
 
-# Build VFX packages
+echo "Build VFX packages"
 docker build -t "usd-docker/vfx:centos7-usd-0.7" -f centos7/vfx/Dockerfile .
 
-# Build USD
+echo "Build USD"
 docker build -t "usd-docker/usd:centos7-usd-0.7.2" -f centos7/usd/Dockerfile .
