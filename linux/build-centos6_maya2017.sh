0a51a95152ebd8f21836febe7331fe5ee763a8ad
Fabrice Macagno <Fabrice.Macagno@al.com.au>
2017-05-22 11:38:35 +1000

    Update build-centos6_maya2017.sh

--- linux/build-centos6_maya2017.sh
+++ linux/build-centos6_maya2017.sh
@@ -5,21 +5,21 @@ echo "Copy local root certificates for corporate networks"
 [ -e /etc/pki/ca-trust/source/anchors ] && cp -u /etc/pki/ca-trust/source/anchors/* cert/
 
 echo "Build base: base centos packages and gcc"
-docker build -t "usd-docker/base:centos7-usd-0.7" -f centos6/base/Dockerfile .
+docker build -t "usd-docker/base:centos6-usd-0.7" -f centos6/base/Dockerfile .
 
 echo "Build VFX packages"
-docker build -t "usd-docker/vfx:centos7-usd-0.7" -f centos6/vfx/Dockerfile .
+docker build -t "usd-docker/vfx:centos6-usd-0.7" -f centos6/vfx/Dockerfile .
 
 echo "Build Maya2017"
 if [ -f ../apps/maya2017/Autodesk_Maya_2017_Update1_P01.tgz -a -f ../apps/maya2017/Maya2017_DEVKIT_Linux.tgz ]; then
   echo "Found Maya2017 tarball"
   mkdir apps
   cp -R ../apps/maya2017/* apps
-  docker build -t "usd-docker/maya2017:centos7-usd-0.7" -f centos6/dcc/Dockerfile_maya2017 .
+  docker build -t "usd-docker/maya2017:centos6-usd-0.7" -f centos6/dcc/Dockerfile_maya2017 .
   rm -Rf apps
 else
   echo "Could not find Maya2017 tarball. Please place mayas tarball and the devkit's tarball into ../apps"
 fi
 
 echo "Build USD"
-docker build -t "usd-docker/usd:centos7-usd-0.7.4" -f centos6/usd/Dockerfile_maya2017 .
+docker build -t "usd-docker/usd:centos6-usd-0.7.4" -f centos6/usd/Dockerfile_maya2017 .
