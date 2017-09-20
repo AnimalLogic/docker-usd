d1e47b140330b23478ede5b54787fd784439c103
Aloys Baillet <aloys.baillet@gmail.com>
2017-09-20 23:09:55 +1000

    Fixed build-centos7_maya2017

--- linux/build-centos7_maya2017.sh
+++ linux/build-centos7_maya2017.sh
@@ -37,18 +37,18 @@ trap finish EXIT
 
 echo "Build base: base centos packages and gcc"
 docker build -t "usd-docker/base:1-centos7" -f centos7/base/Dockerfile .
-docker tag -f "usd-docker/base:1-centos7" "usd-docker/base:latest-centos7"
+docker tag "usd-docker/base:1-centos7" "usd-docker/base:latest-centos7"
 
 echo "Build VFX packages"
 docker build --build-arg current_host_ip_address=$LOCAL_IP -t "usd-docker/vfx:1-centos7" -f centos7/vfx/Dockerfile .
-docker tag -f "usd-docker/vfx:1-centos7" "usd-docker/vfx:latest-centos7"
+docker tag "usd-docker/vfx:1-centos7" "usd-docker/vfx:latest-centos7"
 
 echo "Build Maya2017"
 docker build --build-arg current_host_ip_address=$LOCAL_IP -t "usd-docker/maya2017:1-centos7" -f centos7/dcc/Dockerfile_maya2017 .
-docker tag -f "usd-docker/maya2017:1-centos7" "usd-docker/maya2017:latest-centos7"
+docker tag "usd-docker/maya2017:1-centos7" "usd-docker/maya2017:latest-centos7"
 
 echo "Build USD"
 docker build -t "usd-docker/usd:0.8.0-centos7-maya2017" -f centos7/usd/Dockerfile_maya2017 .
-docker tag -f "usd-docker/usd:0.8.0-centos7-maya2017" "usd-docker/usd:0.8.0-centos7-maya2017"
-docker tag -f "usd-docker/usd:0.8.0-centos7-maya2017" "usd-docker/usd:latest-centos7-maya2017"
-docker tag -f "usd-docker/usd:0.8.0-centos7-maya2017" "usd-docker/usd:latest-centos7-mayalatest"
+docker tag "usd-docker/usd:0.8.0-centos7-maya2017" "usd-docker/usd:0.8.0-centos7-maya2017"
+docker tag "usd-docker/usd:0.8.0-centos7-maya2017" "usd-docker/usd:latest-centos7-maya2017"
+docker tag "usd-docker/usd:0.8.0-centos7-maya2017" "usd-docker/usd:latest-centos7-mayalatest"
